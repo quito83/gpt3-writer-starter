c31f87a4dbe8f0c2d78da1bc533fa857ce95f49b
Quito <quitoapps@gmail.com>
2022-12-01 17:18:30 +0000

    Updates Quito version

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,43 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (event) => {
+    //console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Habla con D10S</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Preguntale al Diego</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Hacele la pregunta que siempre quisiste hacerle!!!</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="Tu pregunta..." 
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Preguntar?</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
